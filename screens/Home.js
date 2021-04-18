53a4b999a4ba6d938adf9cd3832de166aa92dfc6
Nguyen Long <18520092@gm.uit.edu.vn>
2021-04-18 10:01:35 +0700

    Create Tabbar Navigation

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text> Nguyen Thanh Long </Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ screens/PlantDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const PlantDetail = () => {
+  return (
+    <View>
+      <Text> PlantDetail Screen</Text>
+    </View>
+  );
+};
+
+export default PlantDetail;
